85dcab3803f064c7d66cce535778f8bbec5d7bb6
Maria Fernanda <mafenandaup@gmail.com>
2025-02-04 15:11:32 -0300

    test

--- introduction.py
+++ introduction.py
@@ -1,3 +1,14 @@
 # BOOLEANOS EM PYTHON começam sempre com letra maiúscula
 
 # FORMAS DE VARIÁVEIS INVÁLIDAS EM PYTHON
+
+# 1idade   # Começa com um número
+# nome-completo   # Usa um hífen em vez de um sublinhado
+# if   # Palavra-chave reservada do Python
+
+# OPERADORES LOGICOS; and, or, not
+
+# 
+# resultado_and = (a > 5) and (b < 5)   # True
+# resultado_or = (a > 15) or (b < 5)   # True
+# resultado_not = not (a > 5)   # False
