baf7a421d2ff08ac15e67a41ec6104c6fbc49612
Maria Fernanda <mafenandaup@gmail.com>
2025-02-04 18:19:22 -0300

    OK DAMN

--- exercícios_faceis/frooot.py
+++ exercícios_faceis/frooot.py
@@ -1,3 +1,34 @@
 frutas = ["maçã", "banana", "laranja"]
 
-for 
+for index, fruta in enumerate(frutas):
+    print(f"{fruta} ÍNDEX: {index}")
+
+# ao inverso (n consigo tirar do for)
+    print("\nAGORA AO INVERSO\n")
+for index, fruta in enumerate(reversed(frutas)):
+    print(f"{fruta} ÍNDEX INVERSO: {len(frutas) - 1 - index}")
+
+frutas.append("pera")
+print("FRUTA ADICIONADA",frutas)  # Imprime ["maçã", "banana", "laranja", "pera"]
+
+
+frutas.insert(1, "uva")
+print("FRUTA ADD NO INDEX 1",frutas)  # Imprime ["maçã", "uva", "banana", "laranja", "pera"]
+
+
+frutas.remove("banana")
+print("FRUTA REMOVIDA",frutas)  # Imprime ["maçã", "uva", "laranja", "pera"]
+
+
+fruta_removida = frutas.pop(2) # vai "pocar" a fruta no index 2.
+print(frutas)  # Imprime ["maçã", "uva", "pera"]
+print("FRUTA REMOVIDA",fruta_removida)  # Imprime "laranja"
+
+
+frutas.sort()
+print(frutas)  # Imprime ["maçã", "pera", "uva"]
+
+
+frutas.reverse()
+print(frutas)  # Imprime ["uva", "pera", "maçã"]
+
---
+++ palindrome.py
@@ -0,0 +1,8 @@
+import string
+
+
+nome_pessoa = input("\nMe informe qual seu nome e veremos se é um palíndromo: ").lower()
+# aqui coloca em lowercase
+
+for character in nome_pessoa:
+    print('\n' ,character)
