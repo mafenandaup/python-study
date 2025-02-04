a7a0bafdc4bdfbff83f3936cd592d4e62d4e534e
Maria Fernanda <mafenandaup@gmail.com>
2025-02-04 15:21:01 -0300

    n sei

--- introduction.py
+++ introduction.py
@@ -1,3 +1,5 @@
+import random
+
 # BOOLEANOS EM PYTHON começam sempre com letra maiúscula
 
 # FORMAS DE VARIÁVEIS INVÁLIDAS EM PYTHON
@@ -12,3 +14,20 @@
 # resultado_and = (a > 5) and (b < 5)   # True
 # resultado_or = (a > 15) or (b < 5)   # True
 # resultado_not = not (a > 5)   # False
+
+idade = random.randint(1,27)
+tamanho = 1.70
+
+print('sua idade é de: ' ,idade, 'anos.')
+if idade >= 18:
+   print ("Você é maior de idade.")
+else:
+   print('você ainda é de menor')
+
+   if (tamanho < 1.50):
+      print('você é meio que um anão')
+   elif(tamanho == 1.70):
+         print('eh, tá na média')
+   elif(tamanho> 1.70):
+       print('quase um ciclope.')
+      
