b9b8bb3b2ff8838792f4cac3ea951a9e29ef3e80
Maria Fernanda <mafenandaup@gmail.com>
2025-02-05 21:06:01 -0300

    kk

--- exercicios_ok/tupleslice.py
+++ exercicios_ok/tupleslice.py
@@ -1,3 +1,5 @@
 minha_tupla = (1,23,67,80,2,4,4,80,4,80)
 
-print("Os 3 primeiros números:", numeros[:3])
+print("Os 3 primeiros números:", minha_tupla[:3])
+print("Os 3 últimos números:", minha_tupla[:-3])
+print("números da terceira à 8° posição")
