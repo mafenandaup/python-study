679101ad964769d6fc826c4f2362cc6ef60fdec4
Maria Fernanda <mafenandaup@gmail.com>
2025-02-05 21:33:08 -0300

    wow

--- exercicios_ok/greatestsmallest.py
+++ exercicios_ok/greatestsmallest.py
@@ -1,3 +1,12 @@
-numeros =[5,36,78,1,32,11,215,14,0]
+numeros =[5,36,78,1,32,11,215,14,2]
+num_menor = numeros[0]
+num_maior = numeros[0]
 
-for 
+for i in range (1, len(numeros), +1):
+    if numeros[i] < numeros [i-1]:
+        num_menor = numeros[i]
+    if numeros[i] > num_maior:
+        num_maior = numeros[i]
+
+
+print('o menor número da lísta é ', num_menor, ' , e o maior é ', num_maior)
