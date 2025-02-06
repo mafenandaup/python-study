a86336f29e098e7ae95f4e37c0053cbbc61d0a8b
Maria Fernanda <mafenandaup@gmail.com>
2025-02-06 17:05:14 -0300

    dictionarries

--- introduction.py
+++ introduction.py
@@ -86,3 +86,16 @@ print (minha_tupla.index(2, 2))    # devolve o n° de vezes que o número aparec
 
 # pessoa = {"nome": "João", "idade": 25}
 # print(pessoa["nome"])  # João
+# print(pessoa["idade"])    # Imprime 25
+
+# pessoa = {"nome": "João", "idade": 25, "cidade": "Madri"}
+
+
+# print(pessoa.keys())    # Imprime dict_keys(["nome", "idade", "cidade"])
+# print(pessoa.values())  # Imprime dict_values(["João", 25, "Madri"])
+# print(pessoa.items())   # Imprime dict_items([("nome", "João"), ("idade", 25), ("cidade", "Madri")])
+
+
+# pessoa.update({"profissao": "Engenheiro"})
+# print(pessoa)  # Imprime {"nome": "João", "idade": 25, "cidade": "Madri", "profissao": "Engenheiro"}
+
